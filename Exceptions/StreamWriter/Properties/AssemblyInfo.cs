1b30f35b8de220710aa7a8567909f82eae486376
LuGeorgiev <lubomir_78@abv.bg>
2018-03-08 10:35:34 +0200

    Exeptions init. StreamWriter/Reader demos and excercises

---
+++ Exceptions/StreamWriter/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("StreamWriter")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("StreamWriter")]
+[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("d68cec4e-3e5e-4f20-8afd-6ab715a1a8e8")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ Exceptions/SubtitleCorrection/SubtitleCorrection.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SubtitleCorrection
+{
+    class SubtitleCorrection
+    {
+        const double COEFFICIENT = 1.05;
+        const int ADDITION = 5000;
+        const string INPUT_FILE = @"..\..\GORA.txt";
+        const string OUTPUT_FILE = @"..\..\fixdGORA.txt";
+
+        static void Main()
+        {
+            try
+            {
+                var encoding = Encoding.GetEncoding(1251);
+                var streamReader = new StreamReader(INPUT_FILE,encoding);
+                var streamWriter = new StreamWriter(OUTPUT_FILE,true,encoding);
+
+                using (streamReader)
+                {
+                    using (streamWriter)
+                    {
+                        string line;
+                        while ((line=streamReader.ReadLine())!=null)
+                        {
+                            streamWriter.WriteLine(FixLine(line));
+                        }
+                    }
+
+                }
+            }
+            catch (IOException exc)
+            {
+
+                Console.WriteLine("Error: {0}", exc.Message); 
+            }
+
+        }
+
+        static string FixLine(string line)
+        {
+            int bracketFromIndex = line.IndexOf('}');
+            string fromTime = line.Substring(1, bracketFromIndex - 1);
+            int newFromTime = (int)(Convert.ToInt32(fromTime) * COEFFICIENT + ADDITION);
+
+            int bracketToIndex = line.IndexOf('}', bracketFromIndex + 1);
+            string toTime = line.Substring(bracketFromIndex + 2, bracketToIndex - bracketFromIndex - 2);
+            int newToTime = (int)(Convert.ToInt32(toTime) * COEFFICIENT + ADDITION);
+
+            string fixedLine = "{" + newFromTime + "}" + "{" + newToTime + "}" + line.Substring(bracketToIndex + 1);
+
+            return fixedLine;
+        }
+    }
+}
