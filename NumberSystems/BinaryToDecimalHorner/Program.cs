dd4dfe5b29bdaf30bed4d9e2646496bb44fb09d1
LuGeorgiev <lubomir_78@abv.bg>
2018-03-08 10:52:07 +0200

    All type of number system conversion

---
+++ NumberSystems/BinaryToDecimalHorner/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+
+namespace BinaryToDecimalHorner
+{
+    class Program
+    {
+        static void Main()                                  //BinaryToDecimalHorner
+        {
+            string binary = Console.ReadLine();
+            int result = 1;
+            int[] binaryBits = new int[binary.Length];
+            int length = binary.Length;
+
+            for (int i=length-1; i>= 0; i--)
+            {
+                if (binary.EndsWith("1"))
+                {
+                    binaryBits[i] = 1;
+                }
+                else
+                {
+                    binaryBits[i] = 0;
+                }
+
+                binary = binary.Remove(binary.Length - 1);
+            }
+
+            result = binaryBits[0] * 2 + binaryBits[1];
+
+            for (int i = 1; i < length-1; i++)
+            {
+                result = result * 2 + binaryBits[i + 1];
+            }
+            Console.WriteLine(result);
+        }
+    }
+}
---
+++ NumberSystems/Temp/Temp.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Temp
+{
+    class MergeSortAlgorithm
+    {
+        static void Main()
+        {
+            int[] numbers = { 1, -1, 2, -2, 3, -3, 4, -4, 5, -5 };
+            int[] temp = new int[numbers.Length];
+
+            System.Console.WriteLine("Before sorting: {0}", string.Join(" ", numbers));
+
+            MergeSort(numbers, temp, 0, numbers.Length - 1);
+
+            System.Console.WriteLine("After sorting: {0}", string.Join(" ", numbers));
+        }
+
+        static void MergeSort(int[] array, int[] tmp, int start, int end)
+        {
+            // Array with 1 element
+            if (start >= end) return;
+
+            // Define a middle of the array
+            int middle = start + (end - start) / 2;
+
+            MergeSort(array, tmp, start, middle);
+            MergeSort(array, tmp, middle + 1, end);
+
+            CompareAndSort(array, tmp, start, middle, end);
+        }
+
+        static void CompareAndSort(int[] array, int[] tmp, int start, int middle, int end)
+        {
+            int left_tmp = start, left_arr = start, middle_arr = middle + 1;
+
+            while (left_arr <= middle && middle_arr <= end)
+            {
+                if (array[left_arr] > array[middle_arr])
+                {
+                    tmp[left_tmp++] = array[middle_arr++];
+                }
+                else
+                {
+                    tmp[left_tmp++] = array[left_arr++];
+                }
+            }
+
+            while (left_arr <= middle)
+                tmp[left_tmp++] = array[left_arr++];
+
+            while (middle_arr <= end)
+                tmp[left_tmp++] = array[middle_arr++];
+
+            for (int i = start; i <= end; i++)
+                array[i] = tmp[i];
+        }
+    }
+}
