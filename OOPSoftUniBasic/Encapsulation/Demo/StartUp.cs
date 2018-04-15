057635457c71b5b34f122afb3c31c8c509848710
LuGeorgiev <lubomir_78@abv.bg>
2018-04-15 21:20:03 +0300

    Encapsulation

---
+++ OOPSoftUniBasic/Encapsulation/Demo/StartUp.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Demo
+{
+    class StartUp
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
