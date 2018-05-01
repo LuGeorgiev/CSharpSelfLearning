f4615e021b12472a8a4a7ccac0b5cb5e45b26828
Lyubomir Georgiev <LuGeorgiev@globul.bg>
2018-05-01 20:14:49 +0300

    Last problem is similar to previous

---
+++ OOPSoftUniAdvanced/Generics/P10_P11_Tuple/CustTuple.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace P10_P11_Tuple
+{
+    public class CustTuple<T1,T2>
+    {
+        private T1 element1;
+        private T2 element2;
+
+        public CustTuple(T1 elem1, T2 elem2)
+        {
+            this.element1 = elem1;
+            this.element2 = elem2;
+        }
+
+        public override string ToString()
+        {
+            return $"{this.element1} -> {this.element2}";
+        }
+    }
+}
---
+++ OOPSoftUniAdvanced/Generics/P10_P11_Tuple/StartUp.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace P10_P11_Tuple
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            // P11 Threeuple is trivial
+
+            var nameAndAddress = Console.ReadLine().Split();
+            var name = nameAndAddress[0] + " " + nameAndAddress[1];
+            var address = nameAndAddress[2];
+            var nameAddressTuple = new CustTuple<string, string>(name, address);
+            Console.WriteLine(nameAddressTuple);
+
+            var nameLittersBeer = Console.ReadLine().Split();
+            var nickName = nameLittersBeer[0];
+            var litters = int.Parse(nameLittersBeer[1]);
+            var nickBeerTuple = new CustTuple<string, int>(nickName, litters);
+            Console.WriteLine(nickBeerTuple);
+
+            var intDoubl = Console.ReadLine().Split();
+            var intInput =int.Parse( intDoubl[0]);
+            var doubleInput = double.Parse(intDoubl[1]);
+            var intDoubTuple = new CustTuple<int, double>(intInput, doubleInput);
+            Console.WriteLine(intDoubTuple);           
+
+        }
+    }
+}
